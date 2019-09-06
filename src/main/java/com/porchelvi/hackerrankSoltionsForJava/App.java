44da95f5a8436a27a91eb9e333f564d8cc325fe2
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-06 12:20:41 +0200

    testing_Purpose

--- src/main/java/com/porchelvi/hackerrankSoltionsForJava/App.java
+++ src/main/java/com/porchelvi/hackerrankSoltionsForJava/App.java
@@ -7,7 +7,6 @@ package com.porchelvi.hackerrankSoltionsForJava;
 public class App 
 {
 	public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
     }
