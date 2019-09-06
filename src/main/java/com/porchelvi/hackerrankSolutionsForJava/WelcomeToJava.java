d161deac593b43cbf6e0d7017e0f3bb0e9b5838b
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-06 13:29:57 +0200

    HackerRankSolutionsUpdated

--- src/main/java/com/porchelvi/hackerrankSolutionsForJava/WelcomeToJava.java
+++ src/main/java/com/porchelvi/hackerrankSolutionsForJava/WelcomeToJava.java
@@ -1,10 +1,10 @@
-package com.porchelvi.hackerrankSoltionsForJava;
+package com.porchelvi.hackerrankSolutionsForJava;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class WelcomeToJava 
 {
 	public static void main(String[] args) {
         System.out.println("Hello, Java.");
