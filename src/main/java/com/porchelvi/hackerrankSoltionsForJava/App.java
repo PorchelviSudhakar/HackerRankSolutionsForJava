c00aa1d8c7c135cf4d4489d65c4e8fc77a2c80f1
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-06 12:25:21 +0200

    Added two dotss

--- src/main/java/com/porchelvi/hackerrankSoltionsForJava/App.java
+++ src/main/java/com/porchelvi/hackerrankSoltionsForJava/App.java
@@ -7,7 +7,7 @@ package com.porchelvi.hackerrankSoltionsForJava;
 public class App 
 {
 	public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        System.out.println("Hello, World.....");
         System.out.println("Hello, Java.");
     }
 }
