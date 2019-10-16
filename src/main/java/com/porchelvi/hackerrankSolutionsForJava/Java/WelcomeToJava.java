0db97ce1cfca250cdf357d6f83095e326c90a5d0
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-10-16 22:27:07 +0200

    This program simply prints the Hello World

---
+++ src/main/java/com/porchelvi/hackerrankSolutionsForJava/Java/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package com.porchelvi.hackerrankSolutionsForJava.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class WelcomeToJava 
+{
+	public static void main(String[] args) {
+        System.out.println("Hello, Java.");
+    }
+}
