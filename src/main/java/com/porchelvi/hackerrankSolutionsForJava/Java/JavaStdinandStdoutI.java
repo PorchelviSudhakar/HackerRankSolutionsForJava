1bfebb3a5141a9a24ffd11da3e2200e8b18db7ff
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-10-16 22:39:09 +0200

    In this program, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line

--- src/main/java/com/porchelvi/hackerrankSolutionsForJava/Java/JavaStdinandStdoutI.java
+++ src/main/java/com/porchelvi/hackerrankSolutionsForJava/Java/JavaStdinandStdoutI.java
@@ -1,11 +1,13 @@
-package com.porchelvi.hackerrankSolutionsForJava;
+package com.porchelvi.hackerrankSolutionsForJava.Java;
 
 import java.util.Scanner;
 
 public class JavaStdinandStdoutI {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		System.out.println(a);
 		while (scan.hasNext()) {
